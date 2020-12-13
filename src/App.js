72e070f9206822a8919de1fef8235b8216bbd2ac
Yousif Abozid <yousif.abozid@yahoo.com>
2020-12-13 03:15:16 +0200

    initial code

--- src/App.js
+++ src/App.js
@@ -1,95 +1,68 @@
 import React, { useState } from 'react'
-//import './App.css'
-
-// functionality demo: https://codewordsolver.com/
-
-const stringArray = [
-  { word: 'bassoon', score: 371 },
-  { word: 'barroon', score: 371 },
-  { word: 'babboon', score: 371 },
-]
 
 const App = () => {
-  const [results, setResults] = useState([])
-  const [n, setN] = useState([])
-  const [m, setM] = useState([])
-  const input = '1a1122n'
+    const [input, setInput] = useState('')
+    const [result, setResult] = useState([])
+    // this dummy data has all the words in the Engilsh language
+    const dummyData = [
+        '1',
+        'here word with one letter',
+        '22',
+        '333',
+        '4444',
+        '55555',
+        '666666',
+        '7777777',
+        '88888888',
+        '999999999'
+    ]
+    const splitInput = () => {
+        const arrayOfLetters = input.split('')
+        let indexsOfUnkownLetters = []
 
-  const getPlaceholders = (arr) => {
-    const x = [...arr.matchAll(/\d/g)]
-    setN(
-      x.map((item) => {
-        const findPlaceholders = [...arr.match(item[0])]
-        const itemObj = {
-          placeholder: findPlaceholders[0],
-          index: item.index,
-        }
-        return itemObj
-      })
-    )
-  }
-
-  const filter1 = (arr1, arr2) => {
-    console.log('n', n[0])
-    for (const c in n) {
-      console.log('c', c, 'n[c]', n[c])
-      let charWord = arr2.charAt(n[c].index)
-      const r = new RegExp(`${charWord}`, 'g')
-      let z = arr2.match(r)
-      let charInput = arr1.charAt(n[c].index)
-      const rr = new RegExp(`${charInput}`, 'g')
-      let q = arr1.match(rr)
-      console.log('z', z, 'q', q)
-      if (z.length !== q.length) {
-        console.log(
-          'HELP PLEASE SOMEBODY HOW THE F DO I FILTER OUT THE WRONG WORD'
-        )
-      }
-      console.log('----------------------------')
+        arrayOfLetters.map((letter, index) => {
+            if (letter === '?' || letter === '.') {
+                indexsOfUnkownLetters.push(index)
+            }
+            return null
+        })
+        console.log(arrayOfLetters)
+        console.log(indexsOfUnkownLetters)
     }
-  }
+    splitInput()
 
-  const getWordMatches = (arr) => {
-    const words = arr.map((obj) => {
-      console.log('word', obj.word, 'n', n)
-      filter1(input, obj.word)
+    // first we try to make our final result smaller by filtering out the words doesn't match the input length
+    const searchForTheWordsMatch = () => {
+        const matchedByLength = dummyData.filter(word => word.length === input.length)
+        setResult(matchedByLength)
 
-      return obj.word
-    })
-    setM((state) => [...state, ...words])
-  }
 
-  console.log('m', m)
+    }
 
-  console.log('results', results)
+    const handleClick = (event) => {
+        event.preventDefault()
+        console.log('clicked!')
 
-  const handleClick2 = () => {
-    getPlaceholders(input)
-  }
+        // step 1 to make our searching result much more easier by reducing it's size
+        searchForTheWordsMatch()
+    }
 
-  const handleClick3 = () => {
-    getWordMatches(stringArray)
-  }
-
-  return (
-    <div className="App">
-      <button onClick={handleClick2}>get matches</button>
-      <button onClick={handleClick3}>get words</button>
-      {JSON.stringify(n)}
-
-      <br />
-      <div>
-        {m.map((word) => (
-          <div>m word {word} </div>
-        ))}
-      </div>
-
-      <br />
-      <br />
-
-      {JSON.stringify(results)}
-    </div>
-  )
+    // console.log(result)
+    return (
+        <div>
+            <form onSubmit={handleClick}>
+                <input
+                    placeholder='Search for a word'
+                    value={input}
+                    onChange={({ target }) => setInput(target.value)}
+                />
+                <button type='submit'>
+                    Search
+                </button>
+            </form>
+            {result.map((word, index) => <p key={index}>{word}</p>)}
+        </div>
+    )
 }
 
 export default App
